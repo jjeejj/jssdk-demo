46c41418a6c974585bebdb4ccf8ce87770be777f
jjeejj <wenjunjiang1993@163.com>
2017-02-10 16:58:26 +0800

    说明

--- configs/wechat.js
+++ configs/wechat.js
@@ -1,12 +1,11 @@
 
 // 公众号配置信息
 var wechat = {
-    // 灵动的账号
     test:{
         //测试账号
-        appID:'wx4551be47f891ede2',
-        appsecret:'d4624c36b6795d1d99dcf0547af5443d',
-        token:'jjeejj'
+        appID:'',
+        appsecret:'',
+        token:''
     } 
 }
 
